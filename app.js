8388950acbadfd467df5b43c0b485a5ee0063c74
Sunday P. Afolabi <spog4true@gmail.com>
2023-11-08 19:36:51 -0800

    Edit: render data on vue widget using function

--- app.js
+++ app.js
@@ -2,7 +2,8 @@
 const app = Vue.createApp({
     data() {
         return {
-            title: 'The Final Empire'
+            title: 'The Final Empire',
+            author: 'Brandon Sanderson'
         }
     }
 });
