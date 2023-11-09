6a0be008ecf995b357635cd7491992328215a8ab
Sunday P. Afolabi <spog4true@gmail.com>
2023-11-08 16:00:46 -0800

    Add: include app file and instantiate vue object then pass data in the create app

---
+++ app.js
@@ -0,0 +1,7 @@
+// console.log("Hello, Vue.js!");
+const app = Vue.createApp({
+    // root component: here data and/or function can be defined
+    template: '<h2>I am a template</h2>'
+});
+
+app.mount("#app")
