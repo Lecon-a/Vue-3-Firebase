4b6671da5af3f50fd156e0693ecd18162554d1de
Sunday P. Afolabi <spog4true@gmail.com>
2023-11-15 04:09:36 -0800

    Add: add eventhandler

--- app.js
+++ app.js
@@ -3,7 +3,16 @@ const app = Vue.createApp({
     data() {
         return {
             title: 'The Final Empire',
-            author: 'Brandon Sanderson'
+            author: 'Brandon Sanderson',
+            age: 31,
+        }
+    },
+    methods: {
+        changeTitle() {
+            this.title = "Vuejs"
+        },
+        addOne() {
+            this.age++;
         }
     }
 });
