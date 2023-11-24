4e134961c2ca72ed048273adeabf595c41884136
Sunday P. Afolabi <spog4true@gmail.com>
2023-11-23 17:30:40 -0800

    Solve exercise 1: add toggle the isFav value

--- app.js
+++ app.js
@@ -2,11 +2,12 @@
 const app = Vue.createApp({
     data() {
         return {
+            url: "https://github.com/Lecon-a",
             showBook: true,
             books: [
-                {title: 'Open Heaven', author: 'Pastor E.A. Adeboye'},
-                {title: 'The Potency of Historical Event', author: 'Sunday P. Afolabi'},
-                {title: 'The Core of David', author: 'Sunday P. Afolabi'},
+                {title: 'Open Heaven', author: 'Pastor E.A. Adeboye', book_cover: "assets/book1.jpg", isFav: false},
+                {title: 'The Potency of Historical Event', author: 'Sunday P. Afolabi', book_cover: "assets/book1.jpg", isFav: true},
+                {title: 'The Core of David', author: 'Sunday P. Afolabi', book_cover: "assets/book1.jpg", isFav: false},
             ],
             age: 31,
             posX: 0,
@@ -24,7 +25,6 @@ const app = Vue.createApp({
             this.showBook = !this.showBook
         },
         handleEvent(e, data) {
-            console.log(e, e.type);
             if (data) {
                 console.log("Data: ", data);
             }
@@ -32,6 +32,10 @@ const app = Vue.createApp({
         handleMouseMove(e) {
             this.posX = e.offsetX;
             this.posY = e.offsetY;
+        },
+        toggleIsFavourite(index) {
+            //alert("Click")
+            this.books[index].isFav = !this.books[index].isFav
         }
     }
 });
